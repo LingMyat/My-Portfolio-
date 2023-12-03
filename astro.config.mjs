bc915f9f70de516f043f8a65990f86854b539007
LingMyat <bizkits223@gmail.com>
2023-12-03 13:13:51 +0630

    blog init

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,9 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import vue from "@astrojs/vue";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), vue()]
+  integrations: [tailwind(), vue(), sitemap()]
 });
