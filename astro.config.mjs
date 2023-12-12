de3502abfb4a81ca73cf2b159186aaf670825d61
MinnShinSawLingMyat <lma@minshinsaw.com>
2023-12-12 15:57:31 +0630

    Vercel speed

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import vue from "@astrojs/vue";
-
+import { injectSpeedInsights } from '@vercel/speed-insights';
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), vue(), sitemap()]
+  integrations: [tailwind(), vue(), sitemap(), injectSpeedInsights()]
 });
