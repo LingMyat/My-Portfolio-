12f4899567e1b184f986aba96be73a2e1edb651d
LingMyat <bizkits223@gmail.com>
2024-03-17 23:44:20 +0630

    Add React

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,9 @@ import vue from "@astrojs/vue";
 import { injectSpeedInsights } from '@vercel/speed-insights';
 import sitemap from "@astrojs/sitemap";
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), vue(), sitemap(), injectSpeedInsights()]
+  integrations: [tailwind(), vue(), sitemap(), injectSpeedInsights(), react()]
 });
